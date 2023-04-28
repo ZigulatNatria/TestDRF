f714eed2ecf7a4588aeafaee62c86a9a553260c2
ZigulatNatria <vachrameev.oleg@yandex.ru>
2023-04-29 01:50:39 +0500

    finish

--- testdrf/app/views.py
+++ testdrf/app/views.py
@@ -12,11 +12,18 @@ class EntityAPIVew(generics.ListAPIView):
     serializer_class = EntitySerializer
 
 
-class CreateEntityAPI(APIView):
+# class CreateEntityAPI(APIView):
+#
+#     def post(self, request):
+#         serializer = EntitySerializer(data=request.user)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response({'id': serializer.data['id']}, status=status.HTTP_201_CREATED)
+#         return Response({'400': 'Не верно введыны данные'}, status=status.HTTP_400_BAD_REQUEST)
 
-    def post(self, request):
-        serializer = EntitySerializer(data=request.user)
-        if serializer.is_valid():
-            serializer.save()
-            return Response({'id': serializer.data['id']}, status=status.HTTP_201_CREATED)
-        return Response({'400': 'Не верно введыны данные'}, status=status.HTTP_400_BAD_REQUEST)
+
+class CreateEntityAPI(generics.CreateAPIView):
+    serializer_class = EntitySerializer
+
+    def user_create(self, serializer):
+        serializer.save(modified_by=self.request.user)
